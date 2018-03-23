342bef9c9076d917ff1263a59deddea42fe12999
Aravind Natarajan <aravind.natarajan@appian.com>
2018-03-23 14:36:22 -0400

    Got Logging working

--- src/main/java/com/appian/microservices/order/OrderApplication.java
+++ src/main/java/com/appian/microservices/order/OrderApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Inventory application.
@@ -14,8 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderApplication {
 
+  private static Logger LOG = LoggerFactory.getLogger(OrderApplication.class);
+
   @RequestMapping(value = "/orders")
   public String list() {
+    LOG.info("Received request at /orders endpoint");
     return "all the customer's orders";
   }
 
